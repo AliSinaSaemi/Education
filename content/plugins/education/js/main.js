c21207d94c1af52fcaddaa70e93348abb924b70f
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2017-02-28 16:57:22 +0330

    Tabs working and new style of side nav has been included.

--- content/plugins/education/js/main.js
+++ content/plugins/education/js/main.js
@@ -565,6 +565,23 @@ $(document).ready(function () {
   });
 })(jQuery);
 
+// Searcher Code = "2000"
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
 /*
 Please consider that the JS part isn't production ready at all, I just code it to show the concept of merging filters and titles together !
 */
